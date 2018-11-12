de7d22525bb20c5a4517f7a2285719b0cfd4a7c0
djb489 <danieljburgett@gmail.com>
2018-11-11 22:25:34 -0800

    Add files via upload
    changes full_stop.py to only collect data. Analysis will be carried out with a separate script. There's no reason to pull 5,000 days of record every time I run the program.

--- full_stop.py
+++ full_stop.py
@@ -2,39 +2,30 @@ from urllib.request import urlopen
 import json
 import time
 
-# November 1, 0:0:0 = 1541055600
-# October 31, 2018 0:0:0 = 1540969200
-# One day = 86400
-# One week = 604800
-#query_url = "https://api.pushshift.io/reddit/search/comment/?q=%22full%20stop%22&after=1d&before=0d&sort=asc&size=100000"
-
-
-f = open("thot_count_record.txt", "w")
-
+"""
+This file will download json files provided by pushshift api calls
+"""
 after = 1
 before = 0
 
-for x in range(0, 1459):
-    #query_url = "https://api.pushshift.io/reddit/search/comment/?q=%22full%20stop%22&after=" + \
-    #            str(after) + "d&before=" + str(before) + "d&sort=asc&size=100000"
-    query_url = "https://api.pushshift.io/reddit/search/comment/?q=%thot&after=" + \
-                    str(after) + "d&before=" + str(before) + "d&sort=asc&size=100000"
+for day in range(0, 25555):	
+    # Example 'thot' query
+	#query_url = "https://api.pushshift.io/reddit/search/comment/?q=%thot&after=" + \
+    #                str(after) + "d&before=" + str(before) + "d&sort=asc&size=100000"
+	#query_url = "https://api.pushshift.io/reddit/search/comment/?q=%22full%20stop%22&after=1d&before=0d&sort=asc&size=100000"
+	
+    query_url = "https://api.pushshift.io/reddit/search/comment/?q=%22full%20stop%22&after=" + \
+                str(after) + "d&before=" + str(before) + "d&sort=asc&size=100000"
     r = urlopen(query_url)
-
     raw_json = json.load(r)
-    f.write( str( len(raw_json['data']) )+ '\n' )
+
+    mem_file_name = "day_of_record_" + str(day) +".json"
+    print(mem_file_name)
+    with open( mem_file_name,'w') as storage_file:
+        json.dump(raw_json, storage_file)
+    storage_file.close()
 
     after += 1
     before += 1
 
-
-
-f.close()
 print('DONE')
-
-
-
-
-
-
-
